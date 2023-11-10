7227b4d048313f9ff730e2d04e0a575dcf3b2499
namirtek <119343925+namirtek@users.noreply.github.com>
2023-11-10 16:30:02 +0000

    adding 2 verifications : integer / divide by zero

--- calc.py
+++ calc.py
@@ -14,7 +14,27 @@ def multiply(num1, num2):
 
 # Function to divide two numbers
 def divide(num1, num2):
+	check_divide_by_zero(num2)
 	return num1 / num2
+
+def is_number(s):
+    while s.isdigit() == False:
+        s = input("Enter only numbers: ")
+    return int(s)
+
+def check_divide_by_zero(s):
+    while int(s) == 0:
+        s = input("You can't divide by Zero: ")
+    return int(s)
+
+def check_operation_number(s):
+    if s.isdigit() and int(s) >= 1 and int(s) <= 4:
+        return int(s)
+    else:
+        return 0
+
+
+
 
 print("Please select operation -\n" \
 		"1. Add\n" \
@@ -24,25 +44,27 @@ print("Please select operation -\n" \
 
 
 # Take input from the user
-select = int(input("Select operations form 1, 2, 3, 4 :"))
+select_operation_type = check_operation_number(input("Select operations form 1, 2, 3, 4 :"))
 
-number_1 = int(input("Enter first number: "))
-number_2 = int(input("Enter second number: "))
+if select_operation_type > 0:
 
-if select == 1:
-	print(number_1, "+", number_2, "=",
-					add(number_1, number_2))
+    number_1 = is_number(input("Enter first number: "))
+    number_2 = is_number(input("Enter second number: "))
 
-elif select == 2:
-	print(number_1, "-", number_2, "=",
-					subtract(number_1, number_2))
+    if select_operation_type == 1:
+        print(number_1, "+", number_2, "=",
+                        add(number_1, number_2))
 
-elif select == 3:
-	print(number_1, "*", number_2, "=",
-					multiply(number_1, number_2))
+    elif select_operation_type == 2:
+        print(number_1, "-", number_2, "=",
+                        subtract(number_1, number_2))
 
-elif select == 4:
-	print(number_1, "/", number_2, "=",
-					divide(number_1, number_2))
-else:
-	print("Invalid input")
+    elif select_operation_type == 3:
+        print(number_1, "*", number_2, "=",
+                        multiply(number_1, number_2))
+
+    elif select_operation_type == 4:
+        print(number_1, "/", number_2, "=",
+                        divide(number_1, number_2))
+        
+else: print('Bad Operation type')
